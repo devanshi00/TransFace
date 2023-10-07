d8549c24485bef19339194ae22138d885cbd5259
devanshi00 <117909426+devanshi00@users.noreply.github.com>
2023-10-08 00:54:07 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,16 +2,10 @@ import argparse
 import cv2
 import numpy as np
 import torch
-import pickle
+
 from backbones import get_model
-source_images = []
-source_faces = []
 
-with open('source_images.pkl', 'rb') as file:
-    source_images = pickle.load(file)
 
-with open('source_faces.pkl', 'rb') as file:
-    source_faces = pickle.load(file)
 @torch.no_grad()
 def inference(weight, name, images):
     embeddings = []  # Create an empty list to store embeddings
