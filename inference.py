780d0f9e7b41071aca67b3d89f0efed73fce03f5
devanshi00 <117909426+devanshi00@users.noreply.github.com>
2023-10-08 00:49:31 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,37 +1,47 @@
 import argparse
-
 import cv2
 import numpy as np
 import torch
-
+import pickle
 from backbones import get_model
+source_images = []
+source_faces = []
 
+with open('source_images.pkl', 'rb') as file:
+    source_images = pickle.load(file)
 
+with open('source_faces.pkl', 'rb') as file:
+    source_faces = pickle.load(file)
 @torch.no_grad()
-def inference(weight, name, img):
-    if img is None:
-        img = np.random.randint(0, 255, size=(112, 112, 3), dtype=np.uint8)
-    else:
-        img = cv2.imread(img)
-        img = cv2.resize(img, (112, 112))
+def inference(weight, name, images):
+    embeddings = []  # Create an empty list to store embeddings
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = np.transpose(img, (2, 0, 1))
-    img = torch.from_numpy(img).unsqueeze(0).float()
-    img.div_(255).sub_(0.5).div_(0.5)
     net = get_model(name, fp16=False)
     net.load_state_dict(torch.load(weight))
     net.eval()
-    feat = net(img)
-    feat = feat[0].numpy()
-    print(feat)
-    return feat
 
+    for img in images:
+        img = np.array(img)  # Convert PIL image to NumPy array
+        img = cv2.resize(img, (112, 112))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = np.transpose(img, (2, 0, 1))
+        img = torch.from_numpy(img).unsqueeze(0).float()
+        img.div_(255).sub_(0.5).div_(0.5)
+        feat = net(img).numpy()
+        feat = feat[0].numpy()
+        embeddings.append(feat)
+
+    return embeddings
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch ArcFace Training')
     parser.add_argument('--network', type=str, default='r50', help='backbone network')
     parser.add_argument('--weight', type=str, default='')
-    parser.add_argument('--img', type=str, default=None)
     args = parser.parse_args()
-    inference(args.weight, args.network, args.img)
+
+    # Use the modified inference function to get embeddings for all images in source_faces
+    source_embeddings = inference(args.weight, args.network, source_faces)
+
+    # Now, the 'embeddings' list contains embeddings for all images in source_faces
+    print("Embeddings for all images:", source_embeddings)
+
