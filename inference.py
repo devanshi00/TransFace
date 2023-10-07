407f5c60a176c5348f56109cbe7871a789808d3b
devanshi00 <117909426+devanshi00@users.noreply.github.com>
2023-10-08 01:41:19 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -4,14 +4,13 @@ import numpy as np
 import torch
 import pickle
 from backbones import get_model
+
 # Load source_images and source_faces from files
-
 source_faces = []
-
-
 
 with open('source_faces.pkl', 'rb') as file:
     source_faces = pickle.load(file)
+
 @torch.no_grad()
 def inference(weight, name, images):
     embeddings = []  # Create an empty list to store embeddings
@@ -27,7 +26,7 @@ def inference(weight, name, images):
         img = np.transpose(img, (2, 0, 1))
         img = torch.from_numpy(img).unsqueeze(0).float()
         img.div_(255).sub_(0.5).div_(0.5)
-        feat = net(img).numpy()
+        feat = net(img)
         feat = feat[0].numpy()
         embeddings.append(feat)
 
@@ -42,6 +41,13 @@ if __name__ == "__main__":
     # Use the modified inference function to get embeddings for all images in source_faces
     source_embeddings = inference(args.weight, args.network, source_faces)
 
-    # Now, the 'embeddings' list contains embeddings for all images in source_faces
+    # Now, the 'source_embeddings' list contains embeddings for all images in source_faces
     print("Embeddings for all images:", source_embeddings)
+
+    # Save source_embeddings to a file using pickle
+    with open('source_embeddings.pkl', 'wb') as file:
+        pickle.dump(source_embeddings, file)
+
+    print("Source embeddings saved to 'source_embeddings.pkl'")
+
 
